a13f3f87081422d25fd8007a758f8732b78514ad
Taras <tarix2006@gmail.com>
2017-10-24 01:16:34 +0200

    - verbesserung

--- test.py
+++ test.py
@@ -67,19 +67,24 @@ def read():
     trainigfolder = 'C:/Users/Taras/mustererkennungU1/datasource/training/'
     testfile = open(testfilename, 'r')
     print("File for testing is open: ", testfile.name)
-    s = 0
+    errors = 0
     all = 0
+    trainingdict = taketrainingdict(trainigfolder)
+    errorrate = {}
+    for i in range(10):
+        errorrate[i] = 0
     for line in testfile:
         digit = int(line[:1])
         #arr = list(map(lambda x: round(float(x) + 1, 5), line[2:].split(' ')))
         arr = list(map(float, line[2:].split(' ')))
-        trainingdict = taketrainingdict(trainigfolder)
         foundDigit = nnclassifier(arr, trainingdict)
-        if digit == foundDigit:
-            s += 1
+        if digit != foundDigit:
+            errors += 1
+            errorrate[digit] += 1
         all += 1
-    print(s)
-    print(all)
+    print("All tests: ", all)
+    print("Count of all errors: ", errors)
+    print("Error for every Digit: ", errorrate)
 
 
 read()
