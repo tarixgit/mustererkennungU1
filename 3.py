18834e793618f2fe81123ce44a91fafa098168a8
Erwin Zangwill <zenghuipeng@yeah.net>
2017-11-14 20:56:11 +0100

    add  getvarianz

--- 3.py
+++ 3.py
@@ -16,9 +16,9 @@ def getMean(arr):
     mean= sumvector/float(len(arr))
     return mean
     
-def getSstdDev(digitarrs):
-    stdDev= sqrt(sum((digitarrs-getMean(digitarrs))** 2))/float(len(digitarrs))
-    return stdDev
+# def getSstdDev(digitarrs):
+#     stdDev= sqrt(sum((digitarrs-getMean(digitarrs))** 2))/float(len(digitarrs))
+#     return stdDev
 # def getStdDev(digitarrs):
 #     stdDev= sqrt(sum((digitarrs-getMean(digitarrs))** 2))
 #     return stdDev
@@ -29,6 +29,12 @@ def getCovMatr(data, mean):
         vector.shape=(57,1)
         cov = cov + (vector - mean) * transpose(vector - mean)
     return cov/len(data)
+    
+def getVariance(data, mean):
+    var = zeros((57))
+    for vector in data:
+        var= var + (vector - mean )**2
+    return var/len(data)
     
 def getFischerLine(klassetruem, klassefalsem, covklassetrue, covklassefalse):
     return (covklassetrue + covklassefalse )** (-1)*(klassetruem - klassefalsem)
@@ -44,39 +50,39 @@ def getMS(dataArrays, digit):
     
     return 0
     
-def klassifikator(testfilename, trainigfolder, digit1, digit2):
-    digitarrs1= loadTrain(trainigfolder, digit1)
-    digitarrs2= loadTrain(trainigfolder, digit2)
-    mean1=getMean(digitarrs1)
-    mean2=getMean(digitarrs2)
-    stdDev1=getStdDev(digitarrs1)
-    stdDev2=getStdDev(digitarrs2)
-    print(mean1,stdDev1)
-    testfile = open(testfilename, 'r')
-    errors1= 0.0
-    errors2= 0.0
-    all1= 0.0
-    all2= 0.0
-    for line in testfile:
-        digit = int(line[:1])
-        if digit== digit1 or digit== digit2:
-            arr = list(map(float, line[2:].split(' ')))
-            prob1= getProb(mean1, stdDev1, arr)
-            prob2= getProb(mean2, stdDev2, arr)
-            print(prob1,prob2)
-            pre= max(prob1, prob2)
-            if digit== digit1 and pre== prob2:
-                errors1+= 1
-            all1+= 1
-            if digit== digit2 and pre== prob1:
-                errors2+= 1
-            all2+= 1
-    print(digit1,":All tests: ", all1)
-    print("Count of all errors: ", errors1)
-    print("Error for every Digit: ", errors1/all1)
-    print(digit2,":All tests: ", all2)
-    print("Count of all errors: ", errors2)
-    print("Error for every Digit: ", errors2/all2)
+# def klassifikator(testfilename, trainigfolder, digit1, digit2):
+#     digitarrs1= loadTrain(trainigfolder, digit1)
+#     digitarrs2= loadTrain(trainigfolder, digit2)
+#     mean1=getMean(digitarrs1)
+#     mean2=getMean(digitarrs2)
+#     stdDev1=getStdDev(digitarrs1)
+#     stdDev2=getStdDev(digitarrs2)
+#     print(mean1,stdDev1)
+#     testfile = open(testfilename, 'r')
+#     errors1= 0.0
+#     errors2= 0.0
+#     all1= 0.0
+#     all2= 0.0
+#     for line in testfile:
+#         digit = int(line[:1])
+#         if digit== digit1 or digit== digit2:
+#             arr = list(map(float, line[2:].split(' ')))
+#             prob1= getProb(mean1, stdDev1, arr)
+#             prob2= getProb(mean2, stdDev2, arr)
+#             print(prob1,prob2)
+#             pre= max(prob1, prob2)
+#             if digit== digit1 and pre== prob2:
+#                 errors1+= 1
+#             all1+= 1
+#             if digit== digit2 and pre== prob1:
+#                 errors2+= 1
+#             all2+= 1
+#     print(digit1,":All tests: ", all1)
+#     print("Count of all errors: ", errors1)
+#     print("Error for every Digit: ", errors1/all1)
+#     print(digit2,":All tests: ", all2)
+#     print("Count of all errors: ", errors2)
+#     print("Error for every Digit: ", errors2/all2)
 
 def getclasses(train_data):
     vectorlen = len(train_data[0])
@@ -88,6 +94,9 @@ def getclasses(train_data):
         if vector[vectorlen-1] == 0.0:
             resultfalse.append(vector[:vectorlen-1])
     return resulttrue, resultfalse
+    
+def klassifikator():
+    return 0
 
 def gda():
     filename = 'H:/Studium/zweiteSemester/Mustererkennung/Assignment/mustererkennungU1/datasource/spambase.data'
@@ -120,9 +129,13 @@ def gda():
 
     covclasstrue = getCovMatr(classtruearr, classtruem)
     covclassfalse = getCovMatr(classfalsearr, classfalsem)
-
+    
+    
     fisheralpha = getFischerLine(classtruem, classfalsem, covclasstrue, covclassfalse)
-    print(shape(fisheralpha))
+    classtruem = classtruem * fisheralpha
+    classfalsem = classfalsem * fisheralpha
+    vartrue = getVariance(classtruearr, classtruem)
+    varfalse = getVariance(classfalsearr, classfalsem)
     # klassifikator(testfilename, trainigfolder, 3, 5)
     
     
