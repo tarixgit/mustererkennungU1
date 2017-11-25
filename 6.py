8d5a880b2c9aaa59cd699712cec9849ab3fb466d
Tarix <tarix@fu-berlin.de>
2017-11-25 17:38:22 +0100

    split in n and p

--- 6.py
+++ 6.py
@@ -39,11 +39,23 @@ def initalizeClusters(numberofcluster, arr):
         cluster = Cluster([], cov, mean)
         clusters.append(cluster)
     return clusters
+
 def splitArr(arr):
     random.shuffle(arr)
     train_data = arr[:int((len(arr) + 1) * .80)]  # Remaining 80% to training set
     test_data = arr[int(len(arr) * .80 + 1):]  # Splits 20% data to test set
     return train_data, test_data
+
+def getClasses(arr, p_mark, n_mark):
+    vectorlen = len(arr[0])
+    resulttrue = []
+    resultfalse = []
+    for vector in arr:
+        if vector[vectorlen-1] == p_mark:
+            resulttrue.append(vector[:vectorlen-1])
+        if vector[vectorlen-1] == n_mark:
+            resultfalse.append(vector[:vectorlen-1])
+    return resulttrue, resultfalse
 
 def k_means():
     filename = './datasource/iris.data'
@@ -54,11 +66,11 @@ def k_means():
     arr_set_vir = array(setosa_virginica)
     train_set_ver, test_set_ver = splitArr(arr_set_ver)
     train_set_vir, test_set_vir = splitArr(arr_set_vir)
-    guetes = []
-    for i in range(2, 10):
-        clusters = clustering(arr, i)
-        guetes.append(guete(clusters))
-    poltguete(guetes, clusters)
+    #maybe for future
+    #test_set_verP, test_set_verN = getClasses(test_set_ver, 1.0, 2.0)
+    train_set_verP, train_set_verN = getClasses(train_set_ver, 1.0, 2.0)
+    filename = './datasource/iris.data'
+
 
 
 
