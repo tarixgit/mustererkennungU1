f510e5c8c6022d4bd8fb587b999f3bcbfa2bb852
Tarix <tarix@fu-berlin.de>
2017-12-03 20:01:35 +0100

    fixes

--- 7.py
+++ 7.py
@@ -31,11 +31,12 @@ def do_beta_step(beta_before, gamma, dldb):
     return beta_after
 
 def normalization(arr):
-    nor_arr = []
     m, n = np.shape(arr)
+    x0 = np.ones(m)
+    nor_arr = [x0]
     label_arr = arr[:, n - 1]
     for i in range(n - 1):
-        xi = arr[:, i-1] #-1?
+        xi = arr[:, i] #-1?
         mean = np.mean(xi)
         var = np.var(xi)
         nor_arr.append((xi - mean)/var)
@@ -48,19 +49,19 @@ def normalization(arr):
 
 
 def sigmoid_p(beta, x_vector, y): #possibility function
-    return 1.0/1 + np.exp(np.dot(beta, x_vector) * (-y))
+    return 1.0/(1 + np.exp(np.dot(beta, x_vector) * (-y)))
 
-def find_beta(beta0, gamma, train_data, iteration_number):
+def find_beta(train_data, beta0, gamma, iteration_number):
     #
     beta_current = beta0
     train_data_len = len(train_data)
     vector_len = len(train_data[0])
     for i in range(iteration_number):
         #get dldb
-        dldb = np.zeros(vector_len)
+        dldb = np.zeros(vector_len - 1)
         for j in range(train_data_len):
             xi = train_data[j][:vector_len - 1]
-            y = train_data[j][vector_len]
+            y = train_data[j][vector_len - 1]
             correct_vec = y * xi * (1 - sigmoid_p(beta_current, xi, y))
             dldb = dldb + correct_vec
         dldb_step = dldb * gamma
@@ -74,26 +75,37 @@ def classificator(beta, test_data):
     error1 = 0.0
     all2 = 0.0
     error2 = 0.0
-    length = len(test_data)
-    for i in range(length):
-        y = np.dot(beta, test_data[:length- 1])
-        if test_data[length- 1]==1:
+    data_length, vector_len = np.shape(test_data)
+    test_data_full = np.ones((data_length, vector_len + 1))
+    test_data_full[:, 1:] = test_data
+
+    #data_length = len(test_data)
+    #vector_len = len(test_data[0])
+    for i in range(data_length):
+        y = np.dot(beta, test_data_full[i][:vector_len])
+        if test_data_full[i][vector_len] == 1:
             all1 += 1
-            if y<0:
+            if y < 0:
                 error1 += 1
         else:
             all2 += 1
-            if y>=0:
+            if y >= 0:
                 error2 += 1
-    print("p, all vectors: " + str(all1) + " error 1: " + str(error1) + ", n, all vectors: " + str(all2) + " error2: " + str(error2))
+    print("p: " + str(all1) + " error 1: " + str(error1) + ", eee: " + str(error1/all1) + ", n: " + str(all2) + " error2: " + str(error2)+ ", eee: " + str(error2/all2))
 
 
 def logistic_regression():
     filename = './datasource/spambase.data'
     arr = load_data(filename)
     arr = np.array(arr)
-    data_arr = normalization(arr)
-    train_data, test_data = split_arr(data_arr)
+    #data_arr = normalization(arr)
+    train_data, test_data = split_arr(arr)
+    train_data_norm = normalization(train_data)
+    beta0 = intiliaze_beta(train_data_norm)
+    gamma = 10 ** (-5)
+    iteration_number = 300
+    beta = find_beta(train_data_norm, beta0, gamma, iteration_number)
+    classificator(beta, test_data)
 
 
 
