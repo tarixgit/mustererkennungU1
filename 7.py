5fe1dbd1a4743d2376607b56d59826588150a8e9
PENG <zenghuipeng@yeah.net>
2017-12-05 22:28:02 +0100

    add xor_pro

--- 7.py
+++ 7.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random as random
+from sklearn.metrics import confusion_matrix
 
 
 # load the data by one
@@ -79,10 +80,18 @@ def classificator(beta, test_data):
     error1 = 0.0
     all2 = 0.0
     error2 = 0.0
+    x_test = []
+    y_test = []
     data_length, vector_len = np.shape(test_data)
     for i in range(data_length):
+        x = test_data[i][vector_len-1]
         y = np.dot(beta, test_data[i][:vector_len-1])
-        if test_data[i][vector_len-1] == 1:
+        x_test.append(x)
+        if y > 0:
+            y_test.append(1)
+        else:
+            y_test.append(-1)
+        if x == 1:
             all1 += 1
             if y < 0:
                 error1 += 1
@@ -90,6 +99,7 @@ def classificator(beta, test_data):
             all2 += 1
             if y >= 0:
                 error2 += 1
+    print(confusion_matrix(x_test, y_test, labels=[-1, 1]))
     print("Label 1 all test: " + str(all1) + " error: " + str(error1) + ", accuracy: " + str((all1 - error1)/all1)
           + " \nLabel 0 all test : " + str(all2) + " error: " + str(error2) + ", accuracy: " + str((all2 - error2)/all2))
 
@@ -108,4 +118,23 @@ def logistic_regression():
 # Label 1 all test: 360.0 error: 48.0, accuracy: 0.866666666667
 # Label 0 all test : 560.0 error: 31.0, accuracy: 0.944642857143
 
-logistic_regression()
+
+def xor_pro():
+    train_data = np.array(([0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 1]))
+    beta0 = intiliaze_beta(train_data)
+    gamma = 1
+    iteration_number = 10
+    beta = find_beta(train_data, beta0, gamma, iteration_number)
+    classificator(beta, train_data)
+    train_data = np.array(([0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]))
+    beta0 = intiliaze_beta(train_data)
+    beta = find_beta(train_data, beta0, gamma, iteration_number)
+    classificator(beta, train_data)
+    train_data = np.array(([0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]))
+    beta0 = intiliaze_beta(train_data)
+    beta = find_beta(train_data, beta0, gamma, iteration_number)
+    classificator(beta, train_data)
+
+# logistic_regression()
+xor_pro()
+
