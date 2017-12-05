7e31a89df56bcd0d07c3930070d8c7241dd48312
PENG <zenghuipeng@yeah.net>
2017-12-05 21:02:32 +0100

    test_data normalization remove test_data_full in function classificator()

--- 7.py
+++ 7.py
@@ -1,7 +1,8 @@
 import numpy as np
 import random as random
 
-#load the data by one
+
+# load the data by one
 def load_data(filename):
     file = open(filename, 'r')
     digitarrs = []
@@ -14,11 +15,11 @@ def load_data(filename):
 
 # separate training data and rest data (80/20%)
 def split_arr(arr):
-    #TODO: check if this work for list
     np.random.shuffle(arr)
     train_data = arr[:int((len(arr) + 1) * .80)]  # Remaining 80% to training set
     test_data = arr[int(len(arr) * .80 + 1):]  # Splits 20% data to test set
     return train_data, test_data
+
 
 def intiliaze_beta(train_data):
     random.seed()
@@ -26,9 +27,11 @@ def intiliaze_beta(train_data):
     beta = train_data[x]
     return beta[:len(beta) - 1]
 
+
 def do_beta_step(beta_before, gamma, dldb):
     beta_after = beta_before + gamma * dldb
     return beta_after
+
 
 def normalization(arr):
     m, n = np.shape(arr)
@@ -36,7 +39,7 @@ def normalization(arr):
     nor_arr = [x0]
     label_arr = arr[:, n - 1]
     for i in range(n - 1):
-        xi = arr[:, i] #-1?
+        xi = arr[:, i]
         mean = np.mean(xi)
         var = np.var(xi)
         nor_arr.append((xi - mean)/var)
@@ -48,8 +51,10 @@ def normalization(arr):
     return nor_arr
 
 
-def sigmoid_p(beta, x_vector, y): #possibility function
+# possibility function
+def sigmoid_p(beta, x_vector, y):
     return 1.0/(1 + np.exp(np.dot(beta, x_vector) * (-y)))
+
 
 def find_beta(train_data, beta0, gamma, iteration_number):
     #
@@ -57,7 +62,7 @@ def find_beta(train_data, beta0, gamma, iteration_number):
     train_data_len = len(train_data)
     vector_len = len(train_data[0])
     for i in range(iteration_number):
-        #get dldb
+        # get dldb
         dldb = np.zeros(vector_len - 1)
         for j in range(train_data_len):
             xi = train_data[j][:vector_len - 1]
@@ -66,9 +71,8 @@ def find_beta(train_data, beta0, gamma, iteration_number):
             dldb = dldb + correct_vec
         dldb_step = dldb * gamma
         beta_current = do_beta_step(beta_current, gamma, dldb_step)
-        #if beta_after - beta-current < threshold:
-        #    break
     return beta_current
+
 
 def classificator(beta, test_data):
     all1 = 0.0
@@ -76,14 +80,9 @@ def classificator(beta, test_data):
     all2 = 0.0
     error2 = 0.0
     data_length, vector_len = np.shape(test_data)
-    test_data_full = np.ones((data_length, vector_len + 1))
-    test_data_full[:, 1:] = test_data
-
-    #data_length = len(test_data)
-    #vector_len = len(test_data[0])
     for i in range(data_length):
-        y = np.dot(beta, test_data_full[i][:vector_len])
-        if test_data_full[i][vector_len] == 1:
+        y = np.dot(beta, test_data[i][:vector_len-1])
+        if test_data[i][vector_len-1] == 1:
             all1 += 1
             if y < 0:
                 error1 += 1
@@ -91,22 +90,22 @@ def classificator(beta, test_data):
             all2 += 1
             if y >= 0:
                 error2 += 1
-    print("p: " + str(all1) + " error 1: " + str(error1) + ", eee: " + str(error1/all1) + ", n: " + str(all2) + " error2: " + str(error2)+ ", eee: " + str(error2/all2))
+    print("Label 1 all test: " + str(all1) + " error: " + str(error1) + ", accuracy: " + str((all1 - error1)/all1)
+          + " \nLabel 0 all test : " + str(all2) + " error: " + str(error2) + ", accuracy: " + str((all2 - error2)/all2))
 
 
 def logistic_regression():
     filename = './datasource/spambase.data'
     arr = load_data(filename)
     arr = np.array(arr)
-    #data_arr = normalization(arr)
-    train_data, test_data = split_arr(arr)
-    train_data_norm = normalization(train_data)
-    beta0 = intiliaze_beta(train_data_norm)
-    gamma = 10 ** (-5)
-    iteration_number = 300
-    beta = find_beta(train_data_norm, beta0, gamma, iteration_number)
+    data_arr = normalization(arr)
+    train_data, test_data = split_arr(data_arr)
+    beta0 = intiliaze_beta(train_data)
+    gamma = 5 * 10 ** (-3)
+    iteration_number = 1000
+    beta = find_beta(train_data, beta0, gamma, iteration_number)
     classificator(beta, test_data)
-
-
+# Label 1 all test: 360.0 error: 48.0, accuracy: 0.866666666667
+# Label 0 all test : 560.0 error: 31.0, accuracy: 0.944642857143
 
 logistic_regression()
